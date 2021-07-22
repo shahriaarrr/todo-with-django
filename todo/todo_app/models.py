8c8ba0bafc30219cbe9a8558dbdb361a56a3a538
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-07-22 22:21:44 +0430

    add Task's model

--- todo/todo_app/models.py
+++ todo/todo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+class Task(models.Model):
+    PRIORITY = (
+        ("h", "high"),
+        ("l", "low"),
+    )
+    title = models.CharField(max_length=15)
+    Priority = models.CharField(max_length=4, choices=PRIORITY)
+    task_text = models.TextField()
+    date = models.DateTimeField()
+    #rename our manager
+    task = models.Manager()
