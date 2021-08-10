68cd5b6365650eb370289418c89071ee713c1feb
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-10 21:54:13 +0430

    add login form

--- todo/account/forms.py
+++ todo/account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import fields
 
 from .models import *
 
@@ -6,3 +7,9 @@ class Signup(forms.ModelForm):
     class Meta:
         models = User
         fields = ('username', 'first name', 'last name', 'email', 'age', 'gender')
+
+
+class Login(forms.ModelForm):
+    class Meta:
+        models = User
+        fields = ('username', 'password')
