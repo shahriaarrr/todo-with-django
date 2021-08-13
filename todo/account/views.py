d8ef42f35cb9e1c3be00adfbe7723ec2822c8439
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-13 17:00:55 +0430

    build signup view

--- todo/account/views.py
+++ todo/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+from .forms import *
 # Create your views here.
+@csrf_exempt()
+def signup(request):
+    if request.method == "GET":
+        form = Signup()
+        return render(request, 'signup.html', {'form' : form})
+    if request.method == "POST":
+        form = Signup(request.POST)
+        if form.is_valid():
+            user = form.save()
