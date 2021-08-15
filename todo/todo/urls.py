c5095e75a840ab0a769f8839760e916f972a8086
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-15 22:52:25 +0430

    add homeview url

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls'))
+    path('', include('todo_app.urls')),
+    path('account/', include('account.urls')),
 ]
