e4381f41d72929a955f812b13fe31e40f4ec162a
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-16 22:58:46 +0430

    add loginAPI view and url

--- todo/account/api/urls.py
+++ todo/account/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import *
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='signupAPI'),
+    path('login/', view=obtain_auth_token),
 ]
