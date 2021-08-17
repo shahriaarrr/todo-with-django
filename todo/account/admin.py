a4eb98a0267fea8174d3bbebd6bf1f36b063b7f5
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-17 22:27:35 +0430

    add User model to admin panel

--- todo/account/admin.py
+++ todo/account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
+from .models import User
 # Register your models here.
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = (
+        'username',
+        'date_joined',
+        'email',
+        'is_staff',
+        'is_active',
+    )
+    sortable_by = ['username', 'date_joined']
+    list_editable = ['is_staff', 'is_active']
+    search_fields = ['first_name', 'last_name', 'username', 'email']
+    readonly_fields = ['date_joined', 'last_login']
