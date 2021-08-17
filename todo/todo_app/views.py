09b866596c20fe0119c9bcf5e15e80c0b7a2f093
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-17 22:47:02 +0430

    edit view and add base.html and add_task.html

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import TaskForm
 # Create your views here.
+@csrf_exempt
 def home(request):
     return HttpResponse("<h1>Welcom to Todo website</h1>")
 
 def addTask(request):
+    #GET request
     if request.method == "GET":
         form = TaskForm()
-        return render(request, 'signup.html', {'form' : form})
+        return render(request, 'add_task.html', {'form' : form})
+
+    #POST request
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
