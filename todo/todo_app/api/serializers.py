89fe095ac0140d9eb8c494bca7e0d62631a69622
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-08-21 11:34:18 +0430

    add validate functions to task serializer

--- todo/todo_app/api/serializers.py
+++ todo/todo_app/api/serializers.py
@@ -6,3 +6,14 @@ class TaskSerializers(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('title', 'Priority', 'task_text', 'date')
+
+    def validate(self, data):
+        if data.get('title') == data.get('task_text'):
+            raise serializers.ValidationError('The task title and its description can be the same')
+
+    def validate_title(self, value):
+        if len(value) > 10 or len(value) < 4:
+            error='The task title should be a maximum of 10 characters and a minimum of 4 characters'
+            raise serializers.ValidationError(error)
+
+        return value
