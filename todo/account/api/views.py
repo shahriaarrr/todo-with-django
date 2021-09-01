cebae3381f55fc26ca9e6611bb3c3def0a9ccb23
shahriar <shahriaarrr@gmail.com>
2021-09-01 22:39:48 +0430

    edit SignupAPIView classview

--- todo/account/api/views.py
+++ todo/account/api/views.py
@@ -1,3 +1,4 @@
+from account.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -8,7 +9,8 @@ class SignupAPIView(APIView):
     def post(self, request):
         signup_serializer = SignupSerializer(data=request.data)
         if signup_serializer.is_valid():
-            signup_serializer.save()
+            data = signup_serializer.validated_data
+            User.objects.create_user(**data)
             return Response({'message' : 'User successfully signed up'})
 
         return Response({'message' : signup_serializer.errors})
